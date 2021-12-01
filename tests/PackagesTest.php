9ef0b5f24775b32ea6e75fc2755a96e11db62f4f
Ivo Petkov <ivo@ivopetkov.com>
2021-12-01 11:48:36 +0200

    Update PackagesTest.php

--- tests/PackagesTest.php
+++ tests/PackagesTest.php
@@ -18,7 +18,19 @@ class PackagesTest extends BearFramework\AddonTests\PHPUnitTestCase
      */
     public function testPackages()
     {
-        $this->assertTrue(true);
-    }
+        $app = $this->getApp();
 
+        $app->clientPackages
+            ->add('test1', function (IvoPetkov\BearFrameworkAddons\ClientPackage $package) {
+                $package->addJSCode('var a = 5;');
+                $package->get = 'return a;';
+            });
+
+        $html = '<html><head><link rel="client-packages-embed" name="test1"></head><body>content</body></html>';
+        $result = $app->clientPackages->process($html);
+
+        $this->assertTrue(strpos($result, '<script>var clientPackages=') !== false);
+        $this->assertTrue(strpos($result, '<script>clientPackages.__a("test1", "return a;");</script>') !== false);
+        $this->assertTrue(strpos($result, '<body>content</body>') !== false);
+    }
 }
